b433558e2910817925209dc5f22e367b06fcbb63
Cristian Eulises Sanchez Ramirez <taurosanchezramirez@gmail.com>
2023-03-05 23:49:24 -0400

    first homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let binary = num.split("").reverse();
+   let result = 0;
 
-function DecimalABinario(num) {}
+   for (let i = 0; i < binary.length; i++) {
+      result = binary[i] * Math.pow(2, i) + result;
+   }
+
+   return result;
+}
+
+function DecimalABinario(num) {
+   if (num === 0) return "0";
+   let binary = [];
+   while (num / 2 != 0) {
+      binary.push(num % 2);
+      num = parseInt(num / 2);
+   }
+
+   return binary.reverse().join("");
+}
 
 module.exports = {
    BinarioADecimal,
